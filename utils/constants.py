712b585b9c3bc637f90792cf53ced0b0ffb04cd1
dataminer33 <alexandre.f.c.dasilva@gmail.com>
2024-07-27 12:09:29 +0200

    added images, requirementsfile,utils and the streamlit app

---
+++ utils/constants.py
@@ -0,0 +1,15 @@
+# Constants
+EMB_OPENAI_ADA = "text-embedding-ada-002"
+EMB_SBERT_MPNET_BASE = "sentence-transformers/all-mpnet-base-v2" # Chroma takes care if embeddings are None
+EMB_SBERT_MINILM = "sentence-transformers/all-MiniLM-L6-v2" # Chroma takes care if embeddings are None
+EMB_BAAI_V15_LARGE = "BAAI/bge-large-en-v1.5"
+EMB_GTE_BASE = "thenlper/gte-base"
+
+LLM_OPENAI_GPT35 = "gpt-3.5-turbo"
+LLM_OPENAI_GPT4O = "gpt-4o"
+LLM_OPENAI_GPT4O_MINI = "gpt-4o-mini"
+LLM_FLAN_T5_BASE = "google/flan-t5-base"
+LLM_MISTRAL_V03_INSTRUCT = "mistralai/Mistral-7B-Instruct-v0.3"
+LLM_LLAMA3_INSTRUCT = "meta-llama/Meta-Llama-3-8B-Instruct"
+LLM_DISTILBERT = 'distilbert/distilbert-base-uncased'
+
