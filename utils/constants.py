01c6cd60a03f34034e60d6df0bb2b318c0ad0b56
Alexandre Da Silva <alexandre.f.c.dasilva@gmail.com>
2024-07-29 17:50:26 +0200

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,6 @@
 # Constants
-#EMB_BAAI_V15_LARGE = "BAAI/bge-large-en-v1.5"
-EMB_BAAI_V15_LARGE = "thenlper/gte-base"
+EMB_BAAI_V15_LARGE = "BAAI/bge-large-en-v1.5"
+
 
 LLM_OPENAI_GPT35 = "gpt-3.5-turbo"
 LLM_OPENAI_GPT4O = "gpt-4o"
